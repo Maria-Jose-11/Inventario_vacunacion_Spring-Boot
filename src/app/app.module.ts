0f87f5033dc3f1cb5204c859b8e3e74b0283d45e
Maria-Jose-11 <mariajochala@gmail.com>
2022-08-20 20:03:26 -0500

    Crear, actualizar y eliminar usuarios

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,17 +4,49 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsuariosComponent } from './usuarios/usuarios.component';
+import { FormUsuariosComponent } from './usuarios/form-usuarios.component';
+import {Routes, RouterModule} from '@angular/router'
+import { FormsModule } from '@angular/forms';
+
+//configuracion de rutas 
+
+const routes:Routes=[
+  {
+    path:'',
+    redirectTo:'/usuarios',
+    pathMatch:'full'
+  },
+
+  {
+    path:'usuarios',
+    component:UsuariosComponent
+  },
+
+  {
+    path:'usuarios/form',
+    component:FormUsuariosComponent
+  },
+
+  {
+    path:'usuarios/form/:id',
+    component:FormUsuariosComponent
+  },
+
+]
 
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsuariosComponent
+    UsuariosComponent,
+    FormUsuariosComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    RouterModule.forRoot(routes),
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
